44bc56ba027cd93a508bb28098c05a545fd94f6b
SamCarton <sam@samcarton.com>
2014-08-03 18:36:44 +0800

    Added second handler Winder, split to separate layer, some cleanup.

--- src/bat_spindle.c
+++ src/bat_spindle.c
@@ -3,6 +3,11 @@
 
 static Window *window;
 
+// 24/12 Hour Formatting
+static bool is24hrFormat = true;
+const char hourFormat24[] = "%H";
+const char hourFormat12[] = "%I";
+const char minFormat[] = "%M";
 
 static TextLayer *hour_layer;
 static TextLayer *minute_layer;
@@ -15,9 +20,9 @@ const unsigned char textYPadding = 15;
 const unsigned char textXSize = 60; 
 const unsigned char textYSize = 50;
 
-const unsigned char timeTransitionAngle = 90;
 short int timeTransitionedThisTick = 0;
 
+// Spinner stuff
 Layer *spinner_display_layer;
 
 const GPathInfo SPINNER_PATH_POINTS =
@@ -37,17 +42,43 @@ static unsigned char spinAngle = 0;
 
 const unsigned char spinAmount = 5;
 
+// Winder stuff
+Layer *winder_display_layer;
+
+const GPathInfo WINDER_PATH_POINTS =
+{
+  4,
+  (GPoint [])
+    {
+      {-4,10},
+      {4,10},
+      {4,-10},
+      {-4,-10},
+    }
+};
+
+static GPath *winder_path;
+
+const int32_t winder_angle_offset = 45;
+
+static int32_t winder_angle = 360;
+const int32_t winder_angle_delta = 6;
+
+// Time text strings
 static char hour_text_next[] = "00";
 static char hour_text[] = "00";
 static char minute_text_next[] = "00";
 static char minute_text[] = "00";
 
 
-// Change from angle to time-based spin
+// Spin related timing
 static uint32_t currentSpinProgress = 0;
 const uint32_t lengthOfSpin = 2000; //ms
 
-static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed);
+// Forward Declarations
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed);
+static void handle_minute_tick(struct tm *tick_time);
+static void handle_second_tick(struct tm *tick_time);
 static void updateTextLayers();
 static float easingFunction(float p);
 static float backEaseInOut(float p);
@@ -57,6 +88,7 @@ static float bounceEaseOut(float p);
 static void timer_callback(void *data)
 {
   layer_mark_dirty(spinner_display_layer);
+  layer_mark_dirty(winder_display_layer);
   
   currentSpinProgress += timerTick;
   float eased = easingFunction(currentSpinProgress/(lengthOfSpin*1.0f));
@@ -142,6 +174,22 @@ static void spinner_display_layer_callback(Layer *layer, GContext* ctx)
 
   gpath_draw_filled(ctx, spinner_path);
 
+  // draw centre circle
+  struct GPoint centrePoint;
+  centrePoint.x = 72;
+  centrePoint.y = 84;
+  graphics_fill_circle(ctx, centrePoint, 20);  
+
+}
+
+static void winder_display_layer_callback(Layer *layer, GContext* ctx)
+{
+  // switch to white and draw winder
+  graphics_context_set_fill_color(ctx, GColorWhite);
+
+  gpath_rotate_to(winder_path, (TRIG_MAX_ANGLE / 360) * (spinAngle + winder_angle_offset + winder_angle));
+
+  gpath_draw_filled(ctx, winder_path);
 }
 
 static void window_load(Window *window) {
@@ -156,6 +204,15 @@ static void window_load(Window *window) {
   //init spinner path
   spinner_path = gpath_create(&SPINNER_PATH_POINTS);
   gpath_move_to(spinner_path, grect_center_point(&bounds));
+
+  //init winder layer
+  winder_display_layer = layer_create(bounds);
+  layer_set_update_proc(winder_display_layer, winder_display_layer_callback);
+  layer_add_child(window_layer, winder_display_layer);
+
+  //init winder path
+  winder_path = gpath_create(&WINDER_PATH_POINTS);
+  gpath_move_to(winder_path, grect_center_point(&bounds));  
 
   //init hour text layer
   hour_layer = text_layer_create((GRect) { .origin = { textXPadding, textYPadding }, .size = { textXSize, textYSize } });
@@ -181,21 +238,53 @@ static void window_unload(Window *window) {
   text_layer_destroy(hour_layer);
   text_layer_destroy(minute_layer);
   gpath_destroy(spinner_path);
+  gpath_destroy(winder_path);
+  layer_destroy(spinner_display_layer);
+  layer_destroy(winder_display_layer);
 }
 
-static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed)
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
+{
+  if((units_changed & MINUTE_UNIT) != 0)
+  {
+    handle_minute_tick(tick_time);
+  }
+  else
+  {
+    handle_second_tick(tick_time);    
+  }
+}
+
+static void handle_minute_tick(struct tm *tick_time)
 {
   // kick off spin animation
   //layer_mark_dirty(spinner_display_layer);
   timer = app_timer_register(timerTick, timer_callback, NULL);
 
+  // reset winder
+  winder_angle = 360;
+
   // SET MINUTE UPDATE TO OCCUR HALF WAY THRU TRANSITION SO NUMBER CHANGE IS HIDDEN
   timeTransitionedThisTick = 0;
 
   //Save Time strings for updating later  
-  strftime(hour_text_next, sizeof(hour_text_next), "%H", tick_time);
-  strftime(minute_text_next, sizeof(minute_text_next), "%M", tick_time);
+  if(is24hrFormat)
+  {
+    strftime(hour_text_next, sizeof(hour_text_next), hourFormat24, tick_time);
+  }
+  else
+  {
+    strftime(hour_text_next, sizeof(hour_text_next), hourFormat12, tick_time);
+  }
+
+  strftime(minute_text_next, sizeof(minute_text_next), minFormat, tick_time);
   
+}
+
+static void handle_second_tick(struct tm *tick_time)
+{
+  layer_mark_dirty(winder_display_layer);
+  winder_angle = 360 - (tick_time->tm_sec * winder_angle_delta);
 }
 
 static void updateTextLayers()
@@ -208,6 +297,8 @@ static void updateTextLayers()
 
 
 static void init(void) {
+  is24hrFormat = clock_is_24h_style();
+
   window = window_create();
   window_set_background_color(window, GColorWhite);
   window_set_window_handlers(window, (WindowHandlers) {
@@ -217,7 +308,14 @@ static void init(void) {
   const bool animated = true;
   window_stack_push(window, animated);
 
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  tick_timer_service_subscribe(MINUTE_UNIT | SECOND_UNIT, handle_tick);
+
+  // do initial update to get mins to draw on watchface load
+  time_t now;
+  struct tm *nowLocalTime;
+  time(&now);
+  nowLocalTime = localtime(&now);
+  handle_minute_tick(nowLocalTime);
 
 }
 
